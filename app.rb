419a0b5ccb804073355e34b509be5e09a835dd51
Nia Bert <nbert14@gmail.com>
2019-08-16 02:51:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ get '/say/:word1/:word2/:word3/:word4/:word5' do
   @word4 = params[:word4]
   @word5 = params[:word5]
   
-  "#{word1}"
+  "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
 end
 
 end
